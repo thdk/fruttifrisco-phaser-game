33340a45d65ab4bfaf2c2a2386980a3b813d1da5
thdk <t.dekiere@gmail.com>
2017-12-04 10:05:48 +0100

    Add contact materials

--- src/states/game.ts
+++ src/states/game.ts
@@ -1,5 +1,6 @@
 import * as Assets from '../assets';
 import * as Platforms from '../components/platforms';
+import { Physics } from 'phaser-ce';
 
 export default class Title extends Phaser.State {
     private monsters: Phaser.Group;
@@ -7,6 +8,9 @@ export default class Title extends Phaser.State {
 
     private platformCollisionGroup: Phaser.Physics.P2.CollisionGroup;
     private monsterCollisionGroup: Phaser.Physics.P2.CollisionGroup;
+    private monsterMaterial: Physics.P2.Material;
+    private machineMaterial: Physics.P2.Material;
+    private conveyorBeltMaterial: Physics.P2.Material;
 
     public create(): void {
         this.game.physics.startSystem(Phaser.Physics.P2JS);
@@ -14,8 +18,7 @@ export default class Title extends Phaser.State {
         //  Turn on impact events for the world, without this we get no collision callbacks
         // this.game.physics.p2.setImpactEvents(true);
 
-        this.game.physics.p2.restitution = 0;
-
+        this.game.physics.p2.restitution = 0.4;
 
         //  Create our collision groups.
         this.platformCollisionGroup = this.game.physics.p2.createCollisionGroup();
@@ -26,6 +29,37 @@ export default class Title extends Phaser.State {
         //  (which we do) - what this does is adjust the bounds to use its own collision group.
         this.game.physics.p2.updateBoundsCollisionGroup();
 
+        this.monsterMaterial = new Physics.P2.Material('monsterMaterial');
+        this.machineMaterial = new Physics.P2.Material('machineMaterial');
+        this.conveyorBeltMaterial = new Physics.P2.Material('conveyorBeltMaterial');
+
+        var contactMaterial = this.game.physics.p2.createContactMaterial(this.monsterMaterial, this.machineMaterial);
+
+        // leave commented setWorldMaterial for inspiration 
+        //  4 trues = the 4 faces of the world in left, right, top, bottom order
+        // this.game.physics.p2.setWorldMaterial(worldMaterial, true, true, true, true);
+
+        contactMaterial.friction = 0;     // Friction to use in the contact of these two materials.
+        contactMaterial.restitution = 0.3 ;  // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
+        contactMaterial.stiffness = 1e7;    // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
+        contactMaterial.relaxation = 3;     // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
+        contactMaterial.frictionStuffness = 1e7;    // Stiffness of the resulting FrictionEquation that this ContactMaterial generate.
+        contactMaterial.frictionRelaxation = 3;     // Relaxation of the resulting FrictionEquation that this ContactMaterial generate.
+        contactMaterial.surfaceVelocity = 0;        // Will add surface velocity to this material. If bodyA rests on top if bodyB, and the surface velocity is positive, bodyA will slide to the right.
+
+        // leave commented setWorldMaterial for inspiration 
+        //  4 trues = the 4 faces of the world in left, right, top, bottom order
+        // this.game.physics.p2.setWorldMaterial(worldMaterial, true, true, true, true);
+
+        var contactMaterial2 = this.game.physics.p2.createContactMaterial(this.conveyorBeltMaterial, this.monsterMaterial);
+        contactMaterial2.friction = 0;     // Friction to use in the contact of these two materials.
+        contactMaterial2.restitution = 0.2 ;  // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
+        contactMaterial2.stiffness = 1e7;    // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
+        contactMaterial2.relaxation = 3;     // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
+        contactMaterial2.frictionStuffness = 1e7;    // Stiffness of the resulting FrictionEquation that this ContactMaterial generate.
+        contactMaterial2.frictionRelaxation = 3;     // Relaxation of the resulting FrictionEquation that this ContactMaterial generate.
+        contactMaterial2.surfaceVelocity = 5;        // Will add surface velocity to this material. If bodyA rests on top if bodyB, and the surface velocity is positive, bodyA will slide to the right.
+
         const background = this.game.add.image(this.game.world.centerX, this.game.world.centerY, Assets.Images.ImagesBgGame.getName());
         background.anchor.setTo(0.5);
 
@@ -33,23 +67,29 @@ export default class Title extends Phaser.State {
         this.platforms.physicsBodyType = Phaser.Physics.P2JS;
 
         const conveyorBelt = Assets.Images.ImagesConveybelt.getName();
-        this.platforms.create(0, 750, conveyorBelt);
-        this.platforms.create(600, 750, conveyorBelt);
-        this.platforms.create(1200, 750, conveyorBelt);
+
+        const converyerBelt1 = this.platforms.create(0, 750, conveyorBelt);
+        const converyerBelt2 = this.platforms.create(600, 750, conveyorBelt);
+        const converyerBelt3 = this.platforms.create(1200, 750, conveyorBelt);
         const sourceMachine = this.platforms.create(300, 678, Assets.Images.ImagesSourcemachine.getName());
         const tasteMachine = this.platforms.create(900, 678, Assets.Images.ImagesTastemachine.getName());
 
         this.game.physics.p2.enable(this.platforms, true);
-        // this.game.physics.enable(this.platforms, Phaser.Physics.P2JS);
+
+        converyerBelt1.body.setMaterial(this.conveyorBeltMaterial);
+        converyerBelt2.body.setMaterial(this.conveyorBeltMaterial);
+        converyerBelt3.body.setMaterial(this.conveyorBeltMaterial);
 
         // this.platforms.setAll('body.static', true);
         // this.platforms.setAll('body.offset', new Phaser.Point(30, 30));
 
         sourceMachine.body.clearShapes();
         sourceMachine.body.loadPolygon(Assets.JSON.JsonSourcemachine.getName(), Assets.Images.ImagesSourcemachine.getName());
+        (<Phaser.Physics.P2.Body>sourceMachine.body).setMaterial(this.machineMaterial);
 
         const tasteMachineBody: Phaser.Physics.P2.Body = tasteMachine.body;
-        tasteMachineBody.setRectangle(270, 60, 0, -110);
+        const rect = tasteMachineBody.setRectangle(270, 60, 0, -110);
+        rect.material = this.machineMaterial;
 
         this.platforms.forEach((sprite: Phaser.Sprite) => {
             const body: Phaser.Physics.P2.Body = sprite.body;
@@ -57,8 +97,9 @@ export default class Title extends Phaser.State {
             body.setCollisionGroup(this.platformCollisionGroup);
             body.collides(this.monsterCollisionGroup);
             body.static = true;
-            body.debug = true;
+            body.debug = false;
             body.onBeginContact.add(this.monsterDropped);
+
         }, this);
         const body: Phaser.Physics.P2.Body = sourceMachine.body;
         // body.static = true;
@@ -81,20 +122,24 @@ export default class Title extends Phaser.State {
         monster.scale.setTo(0.3);
         monsterAnimation.play();
 
-        this.game.physics.p2.enable(monster, true);
+        this.game.physics.p2.enable(monster);
         const body: Phaser.Physics.P2.Body = monster.body;
         body.fixedRotation = true;
-        body.setCircle(35);
+        const circle = body.setCircle(25);
+        circle.material = this.monsterMaterial;
         body.debug = false;
         body.motionState = Phaser.Physics.P2.Body.DYNAMIC;
-        body.collides(this.platformCollisionGroup, this.monsterDropped, {});
+        body.collides([this.platformCollisionGroup, this.monsterCollisionGroup], this.monsterDropped, {});
         body.setCollisionGroup(this.monsterCollisionGroup);
     }
 
     private monsterDropped(aObject1: any, aObject2: any, context: any) {
-        console.log(context);
         // explode dron and remove missile - kill it, not destroy
-        (<Phaser.Sprite>aObject1.sprite).body.velocity.x = 20;
+        const random_boolean = Math.random() >= 0.5;
+        let direction = -1;
+        if (random_boolean)
+            direction = 1;
+        (<Phaser.Sprite>aObject1.sprite).body.velocity.x = 40 * direction;
         const b: Phaser.Physics.P2.Body = (<Phaser.Sprite>aObject1.sprite).body;
         // (<Phaser.Sprite> aObject2.sprite);
     }
