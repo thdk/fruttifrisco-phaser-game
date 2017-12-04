b0072a66bca00bd022967a2875182469f102af72
thdk <t.dekiere@gmail.com>
2017-12-04 16:20:45 +0100

    make platform classes

--- src/components/platforms.ts
+++ src/components/platforms.ts
@@ -6,12 +6,6 @@ export interface ICollision {
     collides(group: Physics.P2.CollisionGroup | Physics.P2.CollisionGroup[], callback?: Function, callbackContext?: any);
 }
 
-export default class PhysicsP2Platform extends Phaser.Sprite {
-    constructor(game: Phaser.Game, x: number, y: number, key?: string, frame?: string | number) {
-        super(game, x, y, key, frame);
-    }
-}
-
 export class PhysicsSprite extends Phaser.Sprite {
     constructor(game: Phaser.Game, x: number, y: number, key?: string, frame?: string | number) {
         super(game, x, y, key, frame);
@@ -23,6 +17,7 @@ export class PhysicsSprite extends Phaser.Sprite {
 
 export class PhysicsP2Sprite extends PhysicsSprite implements ICollision {
     public body: Physics.P2.Body;
+    protected material: Phaser.Physics.P2.Material;
     constructor(game: Phaser.Game, x: number, y: number, key?: string, frame?: string | number) {
         super(game, x, y, key, frame);
         this.game.physics.p2.enable(this);
@@ -37,33 +32,65 @@ export class PhysicsP2Sprite extends PhysicsSprite implements ICollision {
     }
 }
 
-export class Machine extends PhysicsP2Sprite {
+export class Platform extends PhysicsP2Sprite { 
     constructor(game: Phaser.Game, x: number, y: number, key?: string) {
         super(game, x, y, key);
-        this.body.setMaterial(new Physics.P2.Material('machineMaterial'));
+        this.body.static = true;
+        this.body.debug = true;
+    }
+}
+
+export class Machine extends Platform {   
+    constructor(game: Phaser.Game, x: number, y: number, key?: string) {
+        super(game, x, y, key);
+        this.material = new Physics.P2.Material('machineMaterial');
+        this.body.setMaterial(this.material);
+    }
+
+    public setContactMaterialWith(otherMaterial: Phaser.Physics.P2.Material): Phaser.Physics.P2.ContactMaterial {
+        let contactMaterial: Phaser.Physics.P2.ContactMaterial;
+        switch (otherMaterial.name) {
+            case 'monsterMaterial': {
+                const contactMaterial = this.game.physics.p2.createContactMaterial(this.material, otherMaterial);
+                contactMaterial.friction = 0;     // Friction to use in the contact of these two materials.
+                contactMaterial.restitution = 0.8;  // Restitution (  i.e. how bouncy it is!) to use in the contact of these two materials.
+                contactMaterial.stiffness = 1e7;    // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
+                contactMaterial.relaxation = 3;     // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
+                contactMaterial.frictionStuffness = 1e7;    // Stiffness of the resulting FrictionEquation that this ContactMaterial generate.
+                contactMaterial.frictionRelaxation = 3;     // Relaxation of the resulting FrictionEquation that this ContactMaterial generate.
+                contactMaterial.surfaceVelocity = 0;        // Will add surface velocity to this material. If bodyA rests on top if bodyB, and the surface velocity is positive, bodyA will slide to the right.
+
+                break;
+            }
+            default:
+                return null;
+        }
+
+        return contactMaterial;
     }
 }
 
 export class TasteMachine extends Machine {
     constructor(game: Phaser.Game, x: number, y) {
         super(game, x, y, Assets.Images.ImagesTastemachine.getName());
+        this.body.setRectangle(270, 60, 0, -110);
     }
 }
 
 export class SourceMachine extends Machine {
     constructor(game: Phaser.Game, x: number, y) {
         super(game, x, y, Assets.Images.ImagesSourcemachine.getName());
+        this.body.clearShapes();
+        this.body.loadPolygon(Assets.JSON.JsonSourcemachine.getName(), Assets.Images.ImagesSourcemachine.getName());
     }
 }
 
-export class ConveyerBelt extends PhysicsP2Sprite {
-    private material: Phaser.Physics.P2.Material;
+export class ConveyerBelt extends Platform {   
 
     constructor(game: Phaser.Game, x: number, y) {
         super(game, x, y, Assets.Images.ImagesConveybelt.getName());
 
         this.material = new Phaser.Physics.P2.Material('conveyorBeltMaterial');
-        this.body.static = true;
         this.body.setMaterial(this.material);
     }
 
--- src/states/game.ts
+++ src/states/game.ts
@@ -4,7 +4,6 @@ import { Physics } from 'phaser-ce';
 
 export default class Title extends Phaser.State {
     private monsters: Phaser.Group;
-    private platforms: Phaser.Group;
 
     private platformCollisionGroup: Phaser.Physics.P2.CollisionGroup;
     private monsterCollisionGroup: Phaser.Physics.P2.CollisionGroup;
@@ -29,41 +28,10 @@ export default class Title extends Phaser.State {
         //  (which we do) - what this does is adjust the bounds to use its own collision group.
         this.game.physics.p2.updateBoundsCollisionGroup();
 
-        this.monsterMaterial = new Physics.P2.Material('monsterMaterial');
-        this.machineMaterial = new Physics.P2.Material('machineMaterial');
-
-        const contactMaterial = this.game.physics.p2.createContactMaterial(this.monsterMaterial, this.machineMaterial);
-
-        // leave commented setWorldMaterial for inspiration
-        //  4 trues = the 4 faces of the world in left, right, top, bottom order
-        // this.game.physics.p2.setWorldMaterial(worldMaterial, true, true, true, true);
-
-        contactMaterial.friction = 0;     // Friction to use in the contact of these two materials.
-        // contactMaterial.restitution = 0.3;  // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
-        // contactMaterial.stiffness = 1e7;    // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
-        // contactMaterial.relaxation = 3;     // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
-        // contactMaterial.frictionStuffness = 1e7;    // Stiffness of the resulting FrictionEquation that this ContactMaterial generate.
-        // contactMaterial.frictionRelaxation = 3;     // Relaxation of the resulting FrictionEquation that this ContactMaterial generate.
-        contactMaterial.surfaceVelocity = 0;        // Will add surface velocity to this material. If bodyA rests on top if bodyB, and the surface velocity is positive, bodyA will slide to the right.
-
-        // leave commented setWorldMaterial for inspiration
-        //  4 trues = the 4 faces of the world in left, right, top, bottom order
-        // this.game.physics.p2.setWorldMaterial(worldMaterial, true, true, true, true);
-
-        // const contactMaterial2 = this.game.physics.p2.createContactMaterial(this.conveyorBeltMaterial, this.monsterMaterial);
-        // contactMaterial2.friction = 0;     // Friction to use in the contact of these two materials.
-        // contactMaterial2.restitution = 0.2;  // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
-        // contactMaterial2.stiffness = 1e7;    // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
-        // contactMaterial2.relaxation = 3;     // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
-        // contactMaterial2.frictionStuffness = 1e7;    // Stiffness of the resulting FrictionEquation that this ContactMaterial generate.
-        // contactMaterial2.frictionRelaxation = 3;     // Relaxation of the resulting FrictionEquation that this ContactMaterial generate.
-        // contactMaterial2.surfaceVelocity = 5;        // Will add surface velocity to this material. If bodyA rests on top if bodyB, and the surface velocity is positive, bodyA will slide to the right.
+        this.monsterMaterial = new Physics.P2.Material('monsterMaterial');        
 
         const background = this.game.add.image(this.game.world.centerX, this.game.world.centerY, Assets.Images.ImagesBgGame.getName());
         background.anchor.setTo(0.5);
-
-        this.platforms = this.add.physicsGroup();
-        this.platforms.physicsBodyType = Phaser.Physics.P2JS;
 
         const beltsCoos: number[][] = [[0, 750], [600, 750], [1200, 750]];
         beltsCoos.forEach((coor: number[]) => {
@@ -71,31 +39,17 @@ export default class Title extends Phaser.State {
             belt.setContactMaterialWith(this.monsterMaterial);
             belt.setCollisionGroup(this.platformCollisionGroup);
             belt.collides(this.monsterCollisionGroup);
-            this.platforms.add(belt);
         });
 
-        const sourceMachine = this.platforms.create(300, 678, Assets.Images.ImagesSourcemachine.getName());
-        const tasteMachine = this.platforms.create(900, 678, Assets.Images.ImagesTastemachine.getName());
+        const sourceMachine = new Platforms.SourceMachine(this.game, 300, 678);
+        const tasteMachine = new Platforms.TasteMachine(this.game, 900, 678);
 
-        this.game.physics.p2.enable(this.platforms, true);
+        // this.game.physics.p2.enable(this.platforms, true);
 
-        sourceMachine.body.clearShapes();
-        sourceMachine.body.loadPolygon(Assets.JSON.JsonSourcemachine.getName(), Assets.Images.ImagesSourcemachine.getName());
-        (<Phaser.Physics.P2.Body>sourceMachine.body).setMaterial(this.machineMaterial);
-
-        const tasteMachineBody: Phaser.Physics.P2.Body = tasteMachine.body;
-        const rect = tasteMachineBody.setRectangle(270, 60, 0, -110);
-        rect.material = this.machineMaterial;
-
-        [tasteMachine, sourceMachine].forEach((sprite: Phaser.Sprite) => {
-            const body: Phaser.Physics.P2.Body = sprite.body;
-
-            body.setCollisionGroup(this.platformCollisionGroup);
-            body.collides(this.monsterCollisionGroup);
-            body.static = true;
-            body.debug = false;
-            // body.onBeginContact.add(this.monsterDropped);
-
+        [tasteMachine, sourceMachine].forEach((machine: Platforms.Machine) => {
+            machine.setContactMaterialWith(this.monsterMaterial);
+            machine.setCollisionGroup(this.platformCollisionGroup);
+            machine.collides(this.monsterCollisionGroup);
         }, this);
 
         this.startDropMonsters();
@@ -119,7 +73,7 @@ export default class Title extends Phaser.State {
         body.fixedRotation = true;
         const circle = body.setCircle(25);
         circle.material = this.monsterMaterial;
-        body.debug = false;
+        body.debug = true;
         body.motionState = Phaser.Physics.P2.Body.DYNAMIC;
         body.setCollisionGroup(this.monsterCollisionGroup);
         body.collides([this.platformCollisionGroup, this.monsterCollisionGroup], this.monsterDropped, this);
